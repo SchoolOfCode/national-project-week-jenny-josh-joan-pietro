038501fa7b3d8a383e9961abb5820cd1be58c573
Quinn.C <joanchenuk@gmail.com>
2022-01-18 14:26:06 +0000

    start to divide into createpage and searchpage

--- node-backend/frontend-react/src/components/CreatePage/index.js
+++ node-backend/frontend-react/src/components/CreatePage/index.js
@@ -1,29 +1,39 @@
-import React
- from "react";
- import {Link} from "react-router-dom";
+import React, {useState} from "react";
+ 
 
 function CreatePage() {
+  const initialFormData={
+    topic:"",
+
+  };
+  const [formData, setFormData]= useState(initialFormData);
+  function handleChange(){
+
+  }
+  function handleSubmit(){
+
+  }
      return (
         <>
           <h1>Create your first topic</h1>
 
            <form method='POST' action='submit'>
-           <label for="topic">Topic:</label>
-             <input type='text' id='topic' name='topic'/>
+              <label for="topic">Topic:</label>
+              <input type='text' id='topic' name='topic'/>
               <br></br>
-              <label for="keyword">Keyword:</label>
-             <input type='text' id='keyword' name='keyword'/>
+              <label for="keywords">Keywords:</label>
+              <input type='text' id='keywords' name='keywords'/>
               <br></br>
               <label for="description">Description:</label>
-             <input type='text' id='description' name='description'/>
+              <input type='text' id='description' name='description'/>
               <br></br>
               <label for="link">Link:</label>
-             <input type='text' id='link' name='link'/>
+              <input type='text' id='link' name='link'/>
               <br></br>
               <label for="username">Username:</label>
-             <input type='text' id='username' name='username' />
-             <br></br>
-             <input  type='submit' value='Submit'/>
+              <input type='text' id='username' name='username' />
+              <br></br>
+              <input  type='submit' value='Submit'/>
            </form>
         </>
       );
