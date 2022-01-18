b0f2be72e895ef7ec7c33c4e6311823b76ba086f
Jenny Taylor <jennytaylor82@gmail.com>
2022-01-18 10:03:33 +0000

    test

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.json({ message: "I wish we had some information to give you ☹️" });
+  res.json({ message: "Is this working yet?!" });
 });
 
 export default router;
