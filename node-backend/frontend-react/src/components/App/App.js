edbb77d40274d8de21f1c5653a8a83f668db6903
Pietro Annobil <pietroannobil@Pietros-MacBook-Air.local>
2022-01-18 10:37:35 +0000

    frontend

---
+++ node-backend/frontend-react/src/components/App/App.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import Navbar from '../Navbar'
+import './App.css';
+
+function App() {
+
+  const [data, setData] = useState(null);
+
+useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
+
+  return (
+    <div className="App">
+        <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ node-backend/frontend-react/src/components/Navbar/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div>
+            <h2>hi</h2>
+        </div>
+    )
+}
+
+export default Navbar
