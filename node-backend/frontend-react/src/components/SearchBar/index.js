b84321ef28baaa71f604d33bde3e8a4e02c13a6a
Jenny Taylor <jennytaylor82@gmail.com>
2022-01-19 09:48:57 +0000

    search bar complete

--- node-backend/frontend-react/src/components/SearchBar/index.js
+++ node-backend/frontend-react/src/components/SearchBar/index.js
@@ -1,12 +1,38 @@
-import React from 'react'
+// import e from "express";
+// import res from "express/lib/response";
+import React, { useState } from "react";
+
 
 function SearchBar() {
-    return (
-        <div>
-            <input type='text'/> 
-            <input type='button' value='search'/>
-        </div>
-    )
-}
+  const [text, setText] = useState("");
 
-export default SearchBar
+  const [searchResults, setSearchResults] = useState([])
+
+  async function submitSearch(e) {
+    e.preventDefault();
+    let response = await fetch(`/api/resources?keywords=${text}`);
+    let data = await response.json();
+    console.log(data.payload);
+    setSearchResults(data.payload);
+    setText('')
+  }
+
+  function handleChange(event) {
+    setText(event);
+  }
+  return (
+    <div>
+      <form onSubmit={submitSearch}>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => handleChange(e.target.value)}
+          placeholder="Search by keywords"
+        />
+        <button type="submit">Search</button>
+      </form>
+     
+    </div>
+  );
+}
+export default SearchBar;
