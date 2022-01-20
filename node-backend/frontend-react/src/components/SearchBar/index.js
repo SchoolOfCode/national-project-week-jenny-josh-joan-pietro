d41fb3a79f26135b01cde97333cfe4fce7bf3c37
JoshuaMeah <joshmeah7@gmail.com>
2022-01-20 10:47:23 +0000

    search input required

--- node-backend/frontend-react/src/components/SearchBar/index.js
+++ node-backend/frontend-react/src/components/SearchBar/index.js
@@ -1,26 +1,34 @@
-
 // import e from "express";
 // import res from "express/lib/response";
 import React, { useState } from "react";
-import './SearchBar.css'
+import "./SearchBar.css";
 
-
-function SearchBar({submitSearch, handleChange, handleClick, text}) {
+function SearchBar({ submitSearch, handleChange, handleClick, text }) {
   return (
-    <div className='search-button border border-primary'>
+    <div className="search-button border border-primary">
       <form onSubmit={submitSearch}>
-        <input className='input-search'
+        <input
+          className="input-search"
           type="text"
+          required
           value={text}
           onChange={(e) => handleChange(e.target.value)}
           placeholder="Search by keywords"
         />
-        <button className='input-search-button border border-primary' type="submit">Search</button>
-        <button className='input-search-button border border-primary' onClick={handleClick}>Most Recent Articles</button>
+        <button
+          className="input-search-button border border-primary"
+          type="submit"
+        >
+          Search
+        </button>
+        <button
+          className="input-search-button border border-primary"
+          onClick={handleClick}
+        >
+          Most Recent Articles
+        </button>
       </form>
-     
     </div>
   );
-
 }
 export default SearchBar;
