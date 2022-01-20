31441f6506b1196bfb009e9b708df8a0ef0ad3b1
Jenny Taylor <jennytaylor82@gmail.com>
2022-01-20 12:32:13 +0000

    fix brackets error

--- node-backend/frontend-react/src/components/Home/index.js
+++ node-backend/frontend-react/src/components/Home/index.js
@@ -8,6 +8,7 @@ function Home() {
   //searchbar
   const [searchResults, setSearchResults] = useState([]);
   const [text, setText] = useState("");
+ 
   async function submitSearch(e) {
     e.preventDefault();
     let response = await fetch(`/api/resources?keywords=${text}`);
@@ -20,86 +21,76 @@ function Home() {
       setText("");
     }
   }
+
   function handleChange(event) {
     setText(event);
   }
-  //sidebar
-  // const [topicSearchResults, setTopicSearchResults] = useState([])
-
-
-
-  
- 
   
   function handleSortBy(sortOrder) {
     if (sortOrder === "new to old") {
       setSearchResults([...searchResults].sort((a, b) => a.id - b.id));
     } else {
-setSearchResults([...searchResults].sort((a, b) => b.id - a.id));
-
-  async function searchByTopic(e) {
-    e.preventDefault();
-    let topic = e.target.id;
-    let response;
-    if (topic === "See all resources") {
-      response = await fetch(`/api/resources`);
-    } else {
-      response = await fetch(`/api/resources?topic=${topic}`);
-
+      setSearchResults([...searchResults].sort((a, b) => b.id - a.id));
     }
   }
 
-    let data = await response.json();
-    console.log(data.payload);
-    setSearchResults(data.payload);
+  async function searchByTopic(e) {
+        e.preventDefault();
+        let topic = e.target.id;
+        let response;
+        if (topic === "See all resources") {
+          response = await fetch(`/api/resources`);
+        } else {
+          response = await fetch(`/api/resources?topic=${topic}`);
+        }
+        let data = await response.json();
+        console.log(data.payload);
+        setSearchResults(data.payload);
   }
-  //most recent articles
-  async function handleClick(e) {
-    e.preventDefault();
-    let response = await fetch(`/api/resources?limits=9`);
-    let data = await response.json();
-    console.log("i'm making a recent articles search");
-    console.log(data.payload);
-    setSearchResults(data.payload);
-  }
+  
+      //most recent articles
+      async function handleClick(e) {
+        e.preventDefault();
+        let response = await fetch(`/api/resources?limits=9`);
+        let data = await response.json();
+        console.log("i'm making a recent articles search");
+        console.log(data.payload);
+        setSearchResults(data.payload);
+      }
 
-  return (
-    <>
-      <div>
-        <div className="sideBar-mainContent">
-          <Sidebar searchByTopic={searchByTopic} className="sideBar" />
+      return (
+        <>
+          <div>
+            <div className="sideBar-mainContent">
+              <Sidebar searchByTopic={searchByTopic} className="sideBar" />
 
-          <div className="maincontent">
-            <h1 className="title-page">BOOTCAMPER HELPER</h1>
-            <SearchBar
-              searchResults={searchResults}
-              submitSearch={submitSearch}
-              className="searchBar"
-              handleChange={handleChange}
-              text={text}
-              handleClick={handleClick}
+              <div className="maincontent">
+                <h1 className="title-page">BOOTCAMPER HELPER</h1>
+                <SearchBar
+                  searchResults={searchResults}
+                  submitSearch={submitSearch}
+                  className="searchBar"
+                  handleChange={handleChange}
+                  text={text}
+                  handleClick={handleClick}
+                  handleSortBy={handleSortBy}
+                />
 
-              handleSortBy={handleSortBy}
-      
-        
-
-            />
-
-            {searchResults && (
-              <SearchResult
-                searchResults={searchResults}
-                submitSearch={submitSearch}
-                handleChange={handleChange}
-                text={text}
-                handleClick={handleClick}
-                className="searchResults"
-              />
-            )}
+                {searchResults && (
+                  <SearchResult
+                    searchResults={searchResults}
+                    submitSearch={submitSearch}
+                    handleChange={handleChange}
+                    text={text}
+                    handleClick={handleClick}
+                    className="searchResults"
+                  />
+                )}
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </>
-  );
-}
+        </>
+      );
+    }
 
 export default Home;
